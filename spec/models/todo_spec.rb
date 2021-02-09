64f10afc4ccc2b02e8453b344a49f95e9d895d10
othman <othmanistaiteh@gmail.com>
2021-02-09 18:41:51 +0200

    testing models

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has 0 user at the start" do
+    expect(Todo.count).to eq(0) 
+  end
+  it "has 1 user after #create" do
+    user = User.create(:username => "test123456", :password => "123456789")
+    Todo.create(:user_id=>user.id, :description => "item", :completed => false)
+    expect(Todo.count).to eq(1) 
+  end
+  
+  describe 'associations' do
+    it { should belong_to(:user).class_name('User') }
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "has 0 user at the start" do
+    expect(User.count).to eq(0) 
+  end
+  it "has 1 user after #create" do
+    User.create(:username => "test123456", :password => "123456789")
+    expect(User.count).to eq(1) 
+  end
+  it "does not accept less than 6 characters username" do 
+    User.create(:username => "test1", :password =>"123456789")
+    expect(User.count).to eq(0)
+  end
+  it "does not accept more than 20 characters username" do 
+    User.create(:username => "test123456789123456789123542", :password =>"123456789")
+    expect(User.count).to eq(0)
+  end
+  it "does not store the plan password" do
+    pass = "123456789"
+    user = User.new(:username=> "test123", :password => pass)
+    id = user.id 
+    user.save
+    id = user.id 
+    saved_user = User.find(id)
+    expect(saved_user.password).to_not eq(pass)
+  end
+
+  describe 'associations' do
+    it { should have_many(:todos) }
+  end
 end
